2fb8e1ff98854e2090d813f81c2900d0ea7f7325
AVAVT <itsavavt@gmail.com>
2017-06-11 22:05:35 +0700

    props

--- FE/tk-hotgirl-react/src/components/GirlItem.js
+++ FE/tk-hotgirl-react/src/components/GirlItem.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 
 class GirlItem extends Component {
+  
   render() {
     return (
       <div className="col-sm-6 col-md-3 girl_item">
         <div className="girl_image_container">
-          <img src="./img/1.png" className="girl_image" />
+          <img src={this.props.imageUrl} className="girl_image" />
           <div className="girl_image_overlay">
             <button className="like_button"><span className="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
             <div className="description text-center">
-              <span className="col-xs-4 ">
-                <span className="glyphicon glyphicon-eye-open "aria-hidden="true"></span>
-                <div>857</div>
+              <span className="col-xs-4">
+                <span className="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                <div>{this.props.view}</div>
               </span>
 
               <span className="col-xs-4">
-                <span className="glyphicon glyphicon-time "aria-hidden="true"></span>
-                <div>07/05/12</div>
+                <span className="glyphicon glyphicon-time" aria-hidden="true"></span>
+                <div>{this.props.date}</div>
               </span>
               <span className="col-xs-4">
-                <span className=" glyphicon glyphicon-plus "aria-hidden="true"></span>
-                <div>588</div>
+                <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
+                <div>{this.props.plus}</div>
               </span>
             </div>
           </div>
         </div>
         <div className="girl_item_content">
-          <img src="./img/3.png" className="poster_avatar" />
-          <h3>Dzuuung</h3>
-          <small>HRC photos</small>
-          <p>
-            Aenean vestibulum ut orci nec porta. Cras interdum nisi eu efficitur pellentesque. Nulla at neque vitae mauris lobortis luctus nec nec mi. Fusce non accumsan neque, at viverra orci. Duis ac turpis quis turpis accumsan mattis. Nullam dictum diam quis pharetra dapibus. Nam dignissim enim non velit facilisis, sit amet consectetur mauris tempus. Aenean in lacus vitae enim interdum dignissim.
-          </p>
+          <img src={this.props.posterAvatar} className="poster_avatar" />
+          <h3>{this.props.posterName}</h3>
+          <small>{this.props.posterTitle}</small>
+          <p>{this.props.content}</p>
         </div>
       </div>
     );
